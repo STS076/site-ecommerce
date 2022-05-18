02bb59d80b667ca548009aaf13be0bb4b5a6d657
ZenonStella <stellazenon@gmail.com>
2022-05-18 16:17:20 +0200

    for pull

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let panier = document.getElementById('panier')
 let panierCount = 0
 let vosArticles = document.getElementById('vosArticles')
 let myArticlesArray = []
+let thisArt
 
 
 fetch('dress.json')
@@ -12,7 +13,7 @@ fetch('dress.json')
         dress.forEach(element => {
             fashion.innerHTML += `
                 <div class="card my-2" style="width: 14rem;">
-                    <div id="carousel${element.id}" class="carousel carousel-dark slide" data-bs-ride="carousel" style="width:14rem;">
+                    <div id="carousel-${element.id}" class="carousel carousel-dark slide" data-bs-ride="carousel" style="width:14rem;">
                         <div class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="10000">
                             <img style="width:100%" src="img/${element.imgs[0]}" alt="">
@@ -21,11 +22,11 @@ fetch('dress.json')
                             <img style="width:100%" src="img/${element.imgs[1]}" alt="">
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carousel${element.id}" data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${element.id}" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carousel${element.id}" data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-bs-target="#carousel-${element.id}" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
@@ -34,21 +35,23 @@ fetch('dress.json')
                         <p>${element.name}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="fw-bold">${element.price}€</div>
-                            <button id="article-${element.id}" class="btn p-2 smoll-text" onclick="addToCart(id,${element.id})">Ajouter au panier</button>
+                            <button id="article-${element.id}-btn" class="btn p-2 smoll-text" onclick="addToCart(${element.id})">Ajouter au panier</button>
                         </div>
                     </div>
                 </div>
             `
         });
 
-    })
 
-function addToCart(idBtn, idElement) {
+    })
+function addToCart(idElement) {
     panierCount++
     myArticlesArray.push(idElement)
     // console.log(panierCount);
     panier.innerHTML = "+ " + panierCount
     // Object.values(object1)
-    // myArticlesArray.splice(myArticlesArray.indexOf(element), 1)
     console.log(myArticlesArray);
+    // thisArt = Object.values(json)
+    // dress.splice(dress.indexOf(idElement), 1)
+    console.log(thisArt);
 }
