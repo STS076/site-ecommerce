3d87a0582601f878d438ea6e98293e23f75dd649
STS076 <sophfrom76@hotmail.com>
2022-05-19 11:55:04 +0200

    panier

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 let fashion = document.getElementById('fashion')
-let panier = document.getElementById('panier')
-let panierCount = 0
+let panier = document.getElementById('panier') 
 let vosArticles = document.getElementById('vosArticles')
-let myArticlesArray = []
-let thisArt
+let myCartArray = []
+let allArticlesArray = []
+let panierCount = 0
 
+// panier.innerHTML = "+ " + localStorage.getItem("mycount")
+
+// localStorage.setItem("mycount", panierCount)
+// let nombres = localStorage.getItem("mycount")
+// nombres = nombres.split(",")
+// if (localStorage.getItem("mycount") != ""){
+//     panierCount = localStorage.getItem("mycount")
+// } 
 
 fetch('dress.json')
     .then(response => response.json())
     .then(data => {
+        let count = 0
         let dress = data.results
         dress.forEach(element => {
+            allArticlesArray.push(element)
             fashion.innerHTML += `
                 <div class="card my-2 col-lg-3 col-10 mx-2" >
                     <div id="carousel-${element.id}" class="carousel carousel-dark slide" data-bs-ride="carousel" >
                         <div class="carousel-inner">
                             <div class="carousel-item active" data-bs-interval="10000">
-                            <img style="width:100%" src="img/${element.imgs[0]}" alt="">
+                            <img style="width:100%" src="img/${element.imgs[0]}" alt="vue vêtement de face">
                             </div>
                             <div class="carousel-item" data-bs-interval="2000">
-                            <img style="width:100%" src="img/${element.imgs[1]}" alt="">
+                            <img style="width:100%" src="img/${element.imgs[1]}" alt="vue vêtement de dos">
                             </div>
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${element.id}" data-bs-slide="prev">
@@ -35,24 +45,44 @@ fetch('dress.json')
                         <p>${element.name}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="fw-bold">${element.price}€</div>
-                            <button id="${element.id}-btn" class="btn p-2 smoll-text" onclick="addToCart('${element.id}')">Ajouter au panier</button>
+                            <button id="${element.id}-btn" class="btn p-2 smoll-text" onclick="addToCart(${count})">Ajouter au panier</button>
                         </div>
                     </div>
                 </div>
             `
+            count ++
         });
-
-
     })
 
-function addToCart(idElement) {
+function addToCart(element) {  
     panierCount++
-    // myArticlesArray.push(idElement)
-    console.log(panierCount);
     panier.innerHTML = "+ " + panierCount
-    // Object.values(object1)
-    // console.log(myArticlesArray);
-    // thisArt = Object.values(json)
-    dress.splice(dress.indexOf(idElement), 1)
-    // console.log(thisArt);
+    myCartArray.push(allArticlesArray[element])
+    console.log(myCartArray)
 }
+
+function accessCart() {
+    // let retrieve = localStorage.getItem("monTableau")
+    // retrieve = retrieve.split(",")
+    myCartArray.forEach(element => {
+        vosArticles.innerHTML += `<div class="card mt-1" >
+        <div class="row g-0">
+          <div class="col-lg-4 col-4">
+          <img  style="width:100%"  src="img/${element.imgs[0]}" alt="vêtement dans votre panier">
+          </div>
+          <div class="col-lg-8 col-8">
+            <div class="card-body">
+              <p class="card-title fw-bold fs-5">${element.name}</p>
+              <p class="card-text">${element.price}€</p>
+              
+              <input type="number" class="taille"></input>
+              <button class="btn filtres text-dark fw-bold btn-sm d-flex align-items-end " onclick="deleteItem()">Supprimer</button>
+            </div>
+            
+          </div>
+        </div>
+      </div>`
+    });
+}
+
+
